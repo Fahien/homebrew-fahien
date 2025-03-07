9a5dedc01907d172b4aa047d5bd67ec35f606f05
Antonio Caggiano <info@antoniocaggiano.eu>
2025-03-07 15:45:27 +0100

    vulkan-sdk: version 1.4.304.1

--- Casks/vulkan-sdk.rb
+++ Casks/vulkan-sdk.rb
@@ -1,18 +1,18 @@
-# Copyright (c) 2024, Antonio Caggiano. All rights reserved.
+# Copyright (c) 2024-2025, Antonio Caggiano. All rights reserved.
 # SPDX-License-Identifier: BSD-2-Clause
 
 cask "vulkan-sdk" do
   name "Vulkan SDK"
   desc "The Vulkan SDK enables Vulkan developers to develop Vulkan applications"
   homepage "https://vulkan.lunarg.com/sdk/home"
-  url "https://sdk.lunarg.com/sdk/download/1.4.304.0/mac/vulkansdk-macos-1.4.304.0.zip"
-  sha256 "d8909362c8cd6a61a33f8ab21240c6d3f38c3818f443f52939a9ac198ad0e539"
-  version "1.4.304.0"
+  url "https://sdk.lunarg.com/sdk/download/1.4.304.1/mac/vulkansdk-macos-1.4.304.1.zip"
+  sha256 "db39bb8a0dd2128f7a9b802976dd582e8f9cd20a6c8e36ad3354c70761529526"
+  version "1.4.304.1"
 
   depends_on formula: "python3"
 
   installer script: {
-    executable: "InstallVulkan.app/Contents/MacOS/InstallVulkan",
+    executable: "InstallVulkan-#{version}.app/Contents/MacOS/InstallVulkan-#{version}",
     args: [
       "--root", "#{staged_path}/#{token}", "--accept-licenses", "--default-answer",
       "--confirm-command", "install"
