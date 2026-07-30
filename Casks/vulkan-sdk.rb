30eddd8e083e3882feead2fb9eeae64e90715eea
Antonio Caggiano <info@antoniocaggiano.eu>
2026-07-30 16:17:30 +0200

    vulkan-sdk: version 1.4.357.0

--- Casks/vulkan-sdk.rb
+++ Casks/vulkan-sdk.rb
@@ -5,9 +5,9 @@ cask "vulkan-sdk" do
   name "Vulkan SDK"
   desc "The Vulkan SDK enables Vulkan developers to develop Vulkan applications"
   homepage "https://vulkan.lunarg.com/sdk/home"
-  url "https://sdk.lunarg.com/sdk/download/1.4.350.0/mac/vulkansdk-macos-1.4.350.0.zip"
-  sha256 "7acc181b8fd9b4781bf51ed086222ec95d22004b85b3d0a6683a7e48ca5a1679"
-  version "1.4.350.0"
+  url "https://sdk.lunarg.com/sdk/download/1.4.357.0/mac/vulkansdk-macos-1.4.357.0.zip"
+  sha256 "539433589c83522e6f31b1c7b418a4167e21597a4a361ab119e1dc0760cf3865"
+  version "1.4.357.0"
 
   depends_on formula: "python3"
 
